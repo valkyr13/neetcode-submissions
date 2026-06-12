490d5ccc1af838a9917cc0742fd33d5f55241279
vandana <vandana.swj@gmail.com>
2026-06-12 16:07:43 +0530

    Add: contains-duplicate-ii - submission-6

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-6.py
@@ -0,0 +1,48 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        """
+        so i need a subarray whre first and last el are equal and diff is <=k
+        
+        brute force
+        i have to go through all the subarrays - o()
+        fixed length = k 
+        
+        r++ and check window size
+        is there duplicate at index r -> check map
+
+        window == k 
+        l ++
+        remove l-1 from map
+
+        boundary
+          l = n-1
+            return
+        
+
+        """
+        visited = set()
+        n = len(nums)
+        l = 0
+        r = 1
+        visited.add(nums[l])
+
+        while(r < n):
+            if nums[r] in visited:
+                return True
+
+            visited.add(nums[r])
+            if r-l >= k:
+                visited.remove(nums[l])
+                l += 1
+            r += 1
+            
+        
+        return False
+
+
+
+            
+
+
+
+        
