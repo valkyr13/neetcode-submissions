a9782c2c6a68de8c55d9b7580b31047c40f2b00f
vandana <vandana.swj@gmail.com>
2026-07-01 19:31:40 +0530

    Add: reverse-nodes-in-k-group - submission-2

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        i know how to reverse in place
+        first traverse and check if length is divisible by k
+        need to reverse n//k times
+
+        """
+
+        l = 0
+        start = head
+
+        while (start is not None):
+            start = start.next
+            l += 1
+
+        grp = l//k
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        curr = head
+        nxt = head.next
+        nodes = k
+        prev_tail = dummy
+
+
+        while(grp > 0):
+            start = curr
+            while (nodes > 0):
+                curr.next = prev
+                prev = curr
+
+                curr = nxt
+                if nxt is not None:
+                    nxt = nxt.next
+                
+                nodes -= 1
+            start.next = curr
+            prev_tail.next = prev
+            prev_tail = start
+
+            grp -= 1
+            nodes = k
+
+        return dummy.next
+        
+        
+        
